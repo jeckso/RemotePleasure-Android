41810e1a3debd46967a82c7066a87cdce366d73a
Dima Sokolov <dima.sokolov@mobiline.com>
2021-07-28 18:21:15 +0300

    Added base architecture components.

---
+++ app/src/main/java/com/jeckso/remotepleasure/android/utils/KeyBoardUtils.kt
@@ -0,0 +1,13 @@
+package com.jeckso.remotepleasure.android.utils
+
+import android.view.inputmethod.InputMethodManager
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+
+
+fun FragmentActivity.hideKeyboard() {
+    val inputMethodManager = getSystemService(FragmentActivity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+}
+
+fun Fragment.hideKeyboard() = requireActivity().hideKeyboard()
---
+++ app/src/main/java/com/jeckso/remotepleasure/data/network/rest/services/BaseService.kt
@@ -0,0 +1,8 @@
+package com.jeckso.remotepleasure.data.network.rest.services
+
+interface BaseService {
+
+    companion object {
+        const val KEY_ID = "id"
+    }
+}
