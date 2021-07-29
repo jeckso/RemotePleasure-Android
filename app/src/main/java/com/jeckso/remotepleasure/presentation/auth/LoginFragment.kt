cd5eb9d341a990cdacb1795422da63fb3a782c18
slonevskyi <jeksons11@gmail.com>
2021-07-29 18:34:14 +0300

    Fixed app launch.

--- app/src/main/java/com/jeckso/remotepleasure/presentation/auth/LoginFragment.kt
+++ app/src/main/java/com/jeckso/remotepleasure/presentation/auth/LoginFragment.kt
@@ -2,11 +2,17 @@ package com.jeckso.remotepleasure.presentation.auth
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.jeckso.remotepleasure.R
+import com.jeckso.remotepleasure.data.network.utils.isBadRequest
 import com.jeckso.remotepleasure.databinding.FragmentAuthorizationBinding
 import com.jeckso.remotepleasure.presentation.base.fragment.BaseFragment
-
+import dagger.hilt.android.AndroidEntryPoint
+import retrofit2.HttpException
+import timber.log.Timber
+@AndroidEntryPoint
 class LoginFragment private constructor() :
     BaseFragment<LoginViewModel, FragmentAuthorizationBinding>() {
 
@@ -30,12 +36,31 @@ class LoginFragment private constructor() :
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Timber.e("CREATED")
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Timber.e("CREATED VIEW")
         with(viewBinding) {
             loginButton.setOnClickListener {
                 viewModel.authorize(
                     viewBinding.nameEditText.text.toString()
                 )
             }
+        }
+    }
+    override fun handleError(error: Throwable) {
+        when (error) {
+            is HttpException -> {
+                if (error.isBadRequest) {
+                    showErrorDialog(getString(R.string.title_error))
+                } else {
+                    super.handleError(error)
+                }
+            }
+            else -> super.handleError(error)
         }
     }
 }
--- app/src/main/java/com/jeckso/remotepleasure/presentation/auth/LoginViewModel.kt
+++ app/src/main/java/com/jeckso/remotepleasure/presentation/auth/LoginViewModel.kt
@@ -2,11 +2,12 @@ package com.jeckso.remotepleasure.presentation.auth
 
 import com.jeckso.remotepleasure.domain.usecase.auth.AuthUseCase
 import com.jeckso.remotepleasure.presentation.base.vm.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
-
+@HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val authUseCase: AuthUseCase,
+    private val authUseCase: AuthUseCase
 ) : BaseViewModel() {
 
     fun authorize(login: String) = proceedUi {
